8ab2580fd1866b8f6970f8c62d98ff8b86e26e5f
Painted <will.allen@paintedltd.co.uk>
2014-11-13 16:03:29 +0000

    edited readme file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,22 @@
 class PostsController < ApplicationController
 
 	def new
+		@post = Post.new
 	end
 
 	def create
 		@post = Post.new(post_params)
-
-		@post.save
-		redirect_to @post
+		@post.save!
+		
+		redirect_to posts_path
 	end
 
-	def show
-		@post = Post.find(params[:id])
+	def index
+		@post = Post.all
 	end
 
 	private
 	def post_params
-		params.require(:post).permit(:title, :text)
+		params.require(:post).permit(:title, :text, :image)
 	end
 end
